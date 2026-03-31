aae884a90fcdc4756f320418d7a895a14e88bfbd
Rafael Delgado <rafael.delgado@affinity.pt>
2026-03-31 18:28:14 -0100

    Remove commented-out code and clean up imports in ProgramLoop.java

--- kriolos-efatura-service/src/main/java/io/github/kriolos/efatura/ProgramLoop.java
+++ kriolos-efatura-service/src/main/java/io/github/kriolos/efatura/ProgramLoop.java
@@ -8,6 +8,39 @@ import io.github.kriolos.efatura.clientapi.generated.api.DfeApi;
 import io.github.kriolos.efatura.services.ExportToCsv;
 import io.github.kriolos.efatura.services.FiscalReportService;
 import io.github.kriolos.efatura.services.GetTokenHelper;
+package io.github.kriolos.efatura;
+// import java.io.IOException;
+// import java.util.stream.Stream;
+
+// import io.github.kriolos.efatura.services.GetTokenHelper;
+
+
+// public class ProgramToken {
+
+// 	public static void main (String[] args) throws IOException 
+// 	{
+// 		Stream<String[]> list = Stream.of(args).map(s -> s.split(";"));
+
+// 		for( String [] d : list.toArray(String[][]::new) ) 
+// 		{
+			
+// 			try
+// 			{
+// 				String jwt = GetTokenHelper.init(d[1],d[2]);
+// 				System.out.println(d[1] + "::"+  jwt);
+// 			}
+// 			catch(Exception e ) 
+// 			{
+// 				System.out.println(e.getMessage());
+// 				e.printStackTrace();
+// 			}
+// 			finally 
+// 			{
+// 			}
+			
+// 		}
+// 	}
+// }
 
 
 public class ProgramLoop
